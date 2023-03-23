bd6165815298df62059d439280492088f74ce582
huangqc <hqc2010@gmail.com>
2023-03-23 10:14:44 +0800

    修正生成的类名没有带配置的前缀

--- src/DataTables.GeneratorCore/DataRowTemplate.cs
+++ src/DataTables.GeneratorCore/DataRowTemplate.cs
@@ -33,7 +33,7 @@ namespace DataTables.GeneratorCore
             this.Write("\r\n{\r\n    /// <summary>");
             this.Write(this.ToStringHelper.ToStringWithCulture(GenerationContext.Title));
             this.Write("</summary>\r\n    public sealed class ");
-            this.Write(this.ToStringHelper.ToStringWithCulture(GenerationContext.ClassName));
+            this.Write(this.ToStringHelper.ToStringWithCulture(GenerationContext.RealClassName));
             this.Write(" : DataRowBase\r\n    {\r\n");
  foreach(var item in GenerationContext.Properties) { 
             this.Write("        /// <summary>");

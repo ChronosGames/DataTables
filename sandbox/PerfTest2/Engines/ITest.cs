12783121452290156c8af4a7da691aea27f61cab
huangqc <hqc2010@gmail.com>
2023-05-12 11:33:07 +0800

    整理依赖库

--- sandbox/PerfTest2/Engines/ITest.cs
+++ sandbox/PerfTest2/Engines/ITest.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SQLite;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LiteDB;
 
 namespace TestPerfLiteDB
 {
--- sandbox/PerfTest2/Program.cs
+++ sandbox/PerfTest2/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LiteDB;
-using System.IO;
 
 // based test code -> https://github.com/mbdavid/LiteDB-Perf
 

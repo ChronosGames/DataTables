fd9337f2434128189a4c674d2e2dd93b33157594
huangqc <hqc2010@gmail.com>
2023-06-05 20:11:35 +0800

    新增Unity的单元测试用例

---
+++ src/DataTables.Unity/Assets/Tests/IssueTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace DataTables.Tests
+{
+    public class IssueTest
+    {
+        //[Fact]
+        //public void Issue49()
+        //{
+        //    var builder = new DatabaseBuilder().Append(new[]
+        //    {
+        //        new PersonModel { FirstName = "realname", LastName="reallast" },
+        //        new PersonModel { FirstName = "fakefirst", LastName="fakelast" },
+        //    });
+
+        //    var data = builder.Build();
+        //    var database = new MemoryDatabase(data);
+
+        //    var entries = database.PersonModelTable.FindClosestByFirstNameAndLastName(("real", "real"), false);
+        //    var firstEntry = entries.FirstOrDefault();
+
+        //    var firstIs = firstEntry.FirstName;
+
+        //}
+
+        [Fact]
+        public void Issue57()
+        {
+
+        }
+
+    }
+}
